e0a83a410c0fdfae8c117cc15c2074f68467af95
Gabriel R <grt.araujo@hotmail.com>
2020-10-30 00:16:07 -0300

    Adding other applications.

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ This repository has three applications using GPIO External Interrupts on STM32F4
 ## Introduction
 These projects were developed as a work in the discipline of Embedded Systems Programming at UFMG - Prof. Ricardo de Oliveira Duarte - Department of Electronic Engineering.
 
+## Disabling IRQ
+Simple application to show how to disable external interruptions with the NVIC API. Requires two buttons, a led and some resistors, jumpers and a protoboard.
+
 ## EXTI_priority
-This application describes how to set preemptive priority levels to interrupts. It consists of two push buttons, which will make a LED blink 5 times, but each of them will provide a different blinking frequency for the LED. 
+This application describes how to set preemptive priority levels to interrupts. It consists of two push buttons, which will make a LED blink 5 times, but each of them will provide a different blinking frequency for the LED.
+
+## Time count
+Interesting application for timing actions. This application counts the time between two interruption requests made by the same source and displays the result on an LCD display. To use it, it is necessary to have an LCD display (20x4), a potentiometer, a protobord, an LED and some registers and jumpers.
---
+++ disable_it/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /media/shared/data/STM32_projects/disable_it/Core/Inc/stm32f4xx_hal_conf.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/core_cm4.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_version.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/mpu_armv7.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/media/shared/data/STM32_projects/disable_it/Core/Inc/stm32f4xx_hal_conf.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/core_cm4.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_version.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/media/shared/data/STM32_projects/disable_it/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
